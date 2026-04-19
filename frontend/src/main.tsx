22ec590821e633311dd3841bdb42d12533dfc415
locodigo <cigarrosalitas@gmail.com>
2026-04-19 17:26:22 -0600

    message

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 import { NotificationsProvider } from "@/app/providers/NotificationsProvider";
 import App from "./App";
 import "./index.css";
@@ -46,6 +47,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <NotificationsProvider>
           <App />
+          <Analytics />
           <Toaster
             position="bottom-right"
             toastOptions={{
